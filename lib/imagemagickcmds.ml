c4178664f4474bb7e4e2a2890f51136873432395
Andrew Hannebrink <brink@Andrews-MacBook-Pro.local>
2023-07-12 23:08:04 -0500

    some more imagemagick monkeying around

--- lib/imagemagickcmds.ml
+++ lib/imagemagickcmds.ml
@@ -1,11 +1,21 @@
 open Tile
 
 module Imagemagickcmds = struct 
+  let resolution = (1920, 1080)
   let paint (arrangement : Tile.arrangement) = (
     print_endline ("ImageMagick cmd -- " ^ ((arrangement |> List.hd).file_path |> Option.get));
-    Sys.command "magick -size 1920x1080 xc:skyblue \
-        io/lil_imgs/emoji_buffered/1f34d.png -geometry +0+0 -composite \
-        io/lil_imgs/emoji_buffered/267b-fe0f.png -geometry +50+50 -composite \
-        io/test/result.png"
+    let cmd = ref ("convert -size " ^ 
+        string_of_int (resolution |> fst) ^ "x" ^ 
+        string_of_int (resolution |> snd) ^ " xc:skyblue ") in 
+    List.map (fun (tile : Tile.tile) -> 
+      List.map (fun (coord : Tile.coords) -> (
+        cmd := (!cmd ^ " " ^ (Option.get tile.file_path) ^ " -geometry +" ^ 
+          string_of_int (coord.pixel |> fst) ^ "+" ^ 
+          string_of_int (snd coord.pixel) ^ " -composite ");
+      )) tile.coords
+    ) arrangement |> ignore;
+    cmd := (!cmd ^ " io/test/output.png");
+    print_endline !cmd;
+    Sys.command !cmd
   )
 end
--- lib/mock.ml
+++ lib/mock.ml
@@ -2,11 +2,15 @@ open Tile
 module Mock = struct
   let sample_arrangement: Tile.arrangement = [
     {
-      file_path = Some "assets/emoji1.png";
+      file_path = Some "io/lil_imgs/emoji_buffered/1f34d.png";
       coords = [
         {
           real = Some (0., 0.);
           pixel = (0, 0)
+        };
+        {
+          real = Some (80., 80.);
+          pixel = (80, 80)
         }
       ];
       parent_coords = None;
