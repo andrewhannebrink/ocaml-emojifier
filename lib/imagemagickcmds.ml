280548822d5f441df2d7cbd6d4f741da6841eb79
Andrew Hannebrink <brink@Andrews-MacBook-Pro.local>
2023-07-12 21:38:16 -0500

    imagemagick beginnings. ask not if i should. ask if i coult

--- lib/imagemagickcmds.ml
+++ lib/imagemagickcmds.ml
@@ -1,7 +1,11 @@
 open Tile
 
 module Imagemagickcmds = struct 
-  let paint (arrangement : Tile.arrangement) = 
+  let paint (arrangement : Tile.arrangement) = (
     print_endline ("ImageMagick cmd -- " ^ ((arrangement |> List.hd).file_path |> Option.get));
-    Sys.command "pwd";
+    Sys.command "magick -size 1920x1080 xc:skyblue \
+        io/lil_imgs/emoji_buffered/1f34d.png -geometry +0+0 -composite \
+        io/lil_imgs/emoji_buffered/267b-fe0f.png -geometry +50+50 -composite \
+        io/test/result.png"
+  )
 end
