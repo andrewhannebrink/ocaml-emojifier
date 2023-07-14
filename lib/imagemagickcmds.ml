703a215af21df2dcd1db1bdb8ccb7f4e51d6819b
Andrew Hannebrink <brink@Andrews-MacBook-Pro.local>
2023-07-14 12:27:05 -0500

    magick resizing working. next insert into big mama

--- lib/imagemagickcmds.ml
+++ lib/imagemagickcmds.ml
@@ -2,20 +2,23 @@ open Tile
 
 module Imagemagickcmds = struct 
   let resolution = (1920, 1080)
+
   let paint (arrangement : Tile.arrangement) = (
     print_endline ("ImageMagick cmd -- " ^ ((arrangement |> List.hd).file_path |> Option.get));
-    let cmd = ref ("convert -size " ^ 
+    let cmd = ref ("magick -size " ^ 
         string_of_int (resolution |> fst) ^ "x" ^ 
         string_of_int (resolution |> snd) ^ " xc:skyblue ") in 
     List.map (fun (tile : Tile.tile) -> 
       List.map (fun (coord : Tile.coords) -> (
-        cmd := (!cmd ^ " " ^ (Option.get tile.file_path) ^ " -geometry +" ^ 
+        cmd := (!cmd ^ " " ^ (Option.get tile.file_path) ^ " -geometry " ^ 
+          (tile.depth |> Float.round |> int_of_float |> string_of_int) ^ "x" ^ 
+          (tile.depth |> Float.round |> int_of_float |> string_of_int) ^ "+" ^
           string_of_int (coord.pixel |> fst) ^ "+" ^ 
-          string_of_int (snd coord.pixel) ^ " -composite ");
+          string_of_int (snd coord.pixel) ^ " -composite")
       )) tile.coords
     ) arrangement |> ignore;
     cmd := (!cmd ^ " io/test/output.png");
-    print_endline !cmd;
+    print_endline (!cmd);
     Sys.command !cmd
   )
 end
--- lib/mock.ml
+++ lib/mock.ml
@@ -1,5 +1,6 @@
 open Tile 
 open Mosaic
+open Imagemagickcmds
 
 module Mock = struct
   let sample_arrangement: Tile.arrangement = [
@@ -22,10 +23,43 @@ module Mock = struct
   ]
   let unit_arrangement: Tile.arrangement = (
     let lil_imgs = Mosaic.get_lil_imgs_from_dir "emoji_buffered" in
+    let n = lil_imgs |> List.length |> float_of_int in 
+    let vx = Imagemagickcmds.resolution |> fst |> float_of_int in
+    let vy = Imagemagickcmds.resolution |> snd |> float_of_int in
+    let px = (n *. vx /. vy) |> sqrt |> ceil in
+    let py = (n *. vy /. vx) |> sqrt |> ceil in
+    let sx = if (px *. vy /. vx) *. px < n then
+      (vy /. (px *. vy /. vx)) |> ceil else
+      (vx /. px) |> ceil in
+    let sy = if (py *. vx /. vy) *. py < n then
+      (vx /. (py *. vx /. vy)) |> ceil else
+      (vy /. py) |> ceil in 
+    let i = ref 0 in 
+    let op_arrangement = ref [] in
+    let _ = (List.init (int_of_float px) (fun x -> x))
+    |> List.map (fun x -> (
+      List.init (int_of_float py) (fun y -> y)
+      |> List.map (fun y -> 
+        (string_of_int x ^ " | " ^
+          string_of_int y ^ " | " ^
+          string_of_float sx ^ " | " ^
+          string_of_float sy)
+        |> print_endline;
+        let tile = Tile.make_tile 
+            (Some ("io/lil_imgs/emoji_buffered/" ^ (List.nth lil_imgs !i)))
+            (float_of_int x *. sx)
+            (float_of_int y *. sx)
+            sx in 
+        op_arrangement := tile :: !op_arrangement;
+      )
+    )) in
+    !op_arrangement
+    (*
     List.map (fun lil_img ->
       print_endline lil_img;
         Tile.make_tile (Some ("io/lil_imgs/emoji_buffered/" ^ lil_img))
             200. 200. 64.
       ) lil_imgs
+      *)
   )
 end
